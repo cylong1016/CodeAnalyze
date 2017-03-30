d9a7125328ec7ce4bbf13a00fff0929d121401de
cylong1016 <cyl19941016@gmail.com>
2017-03-31 00:02:23 +0800

    delete nav.jsp

--- src/main/java/edu/nju/controller/checkstyle/CheckStyleController.java
+++ src/main/java/edu/nju/controller/checkstyle/CheckStyleController.java
@@ -3,8 +3,6 @@ package edu.nju.controller.checkstyle;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by floyd on 2017/3/21.
