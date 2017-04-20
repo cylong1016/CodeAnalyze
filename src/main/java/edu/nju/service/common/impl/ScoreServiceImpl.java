37c38673729e8416daf33fc43f6fa55ba75f5c67
lsySummer <864986781@qq.com>
2017-04-20 20:59:10 +0800

    修改了获得全部得分的方法

--- src/main/java/edu/nju/service/common/impl/ScoreServiceImpl.java
+++ src/main/java/edu/nju/service/common/impl/ScoreServiceImpl.java
@@ -1,5 +1,13 @@
 package edu.nju.service.common.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import edu.nju.Vo.common.GroupAllScore;
 import edu.nju.dao.CheckDao;
 import edu.nju.dao.StudentScoreDao;
@@ -8,12 +16,6 @@ import edu.nju.entities.CheckLog;
 import edu.nju.entities.StudentScore;
 import edu.nju.entities.TeacherScore;
 import edu.nju.service.common.ScoreService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by Administrator on 2017/4/18.
@@ -34,19 +36,36 @@ public class ScoreServiceImpl implements ScoreService {
         Map<String, Object> querys = new HashMap<>();
         querys.put("groupId", groupId);
         List<CheckLog> checks = checkDao.getAllCheck();
+        List<TeacherScore> tscores=new ArrayList<TeacherScore>();
+        List<StudentScore> pmdscores=new ArrayList<StudentScore>();
+        List<StudentScore> checkScores=new ArrayList<StudentScore>();
         for(CheckLog item : checks){
+        	long checkId = item.getId();
             groupScores.addCheckDate(item.getCheckDate());
+            querys=new HashMap<>();
+            querys.put("groupId", groupId);
+            querys.put("checkId", checkId);
+	        List<TeacherScore> tempScores = teacherScoreDao.getTeacherScoreByQuery(querys);
+	        querys.put("toolName", "pmd");
+	        List<StudentScore> tempPmd = studentScoreDao.getStudentScoreByQuery(querys);
+	        querys.replace("toolName", "checkstyle");
+	        List<StudentScore> tempCheck = studentScoreDao.getStudentScoreByQuery(querys);
+            if(tempScores.size()>0&&tempPmd.size()>0&&tempCheck.size()>0){
+            	tscores.add(tempScores.get(0));
+            	pmdscores.add(tempPmd.get(0));
+            	checkScores.add(tempCheck.get(0));
+            }
         }
-        List<TeacherScore> tscores = teacherScoreDao.getTeacherScoreByQuery(querys);
-        List<StudentScore> sscores = studentScoreDao.getStudentScoreByQuery(querys);
         for(TeacherScore item : tscores){
-            groupScores.addTeacherScore(item.getScore());
+        	groupScores.addTeacherScore(item.getScore());
         }
-        for(StudentScore item : sscores){
-            groupScores.addCheckstyleScore(item.getCheckstyleScore());
-            groupScores.addPmdScore(item.getPmdScore());
-            groupScores.addSqScore(item.getSqScore());
+        for(StudentScore item : pmdscores){
+        	groupScores.addPmdScore(item.getScore());
         }
+        for(StudentScore item : checkScores){
+        	groupScores.addCheckstyleScore(item.getScore());
+        }
+        
         return groupScores;
     }
 }
